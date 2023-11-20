203a4baf5d0ef1d9736b8394b4085ab8ef28a141
“Mykola <nickolasn717@gmail.com>
2023-11-20 19:36:24 +0200

    add my first homework

---
+++ python_homework_1.py
@@ -0,0 +1,2 @@
+description_of_my_favorite_city = "My favorite city Odessa. September 2, 1794 founded the city of Odessa. In Odessa - this is the main street of the city - Deribasovskaya."
+print(description_of_my_favorite_city)
